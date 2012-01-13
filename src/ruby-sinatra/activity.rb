74c9535474c70d4835c5467845827c359a7ec505
Phil Leggetter <phil@leggetter.co.uk>
2012-01-13 00:46:45 +0000

    adding ruby example

---
+++ src/ruby-sinatra/activity.rb
@@ -0,0 +1,90 @@
+require 'digest/md5'
+require 'uuid'
+
+class Activity
+  
+  def initialize(activity_type, action_text, options)
+    
+    @options = set_default_options(options)
+      
+    @type = activity_type
+    @id = UUID.new.generate
+    @date = Time.now.strftime('%Y-%m-%d %H:%M:%S')
+    
+    @action_text = action_text;
+    @display_name = options["displayName"]
+    @image = options['image'];
+    
+    if( options['get_gravatar'] &&
+        options['email'] )
+         
+      @image["url"] = get_gravatar(options['email'])
+      
+    end
+    
+  end
+  
+  def getMessage() 
+    activity = {
+      'id' => @id,
+      'body' => @action_text,
+      'published' => @date,
+      'type' => @type,
+      'actor' => {
+        'displayName' => @display_name,
+        'objectType' => 'person',
+        'image' => @image
+      }
+    }
+    return activity
+  end
+  
+  def set_default_options(options)
+    defaults = {
+      'email' => nil,
+      'displayName' => nil,
+      'image' => {
+          'url' => 'http://www.gravatar.com/avatar/00000000000000000000000000000000?d=wavatar&s=48',
+          'width' => 48,
+          'height' => 48
+       }
+    }
+    
+    defaults.each {
+      |key, value|
+      if( !options[key] )
+        options[key] = value
+      end
+    }
+    
+    return options
+  end
+  
+  # from: http://en.gravatar.com/site/implement/images/php/
+  # 
+  # Get either a Gravatar URL or complete image tag for a specified email address.
+  # 
+  # @param string $email The email address
+  # @param string $s Size in pixels, defaults to 80px [ 1 - 512 ]
+  # @param string $d Default imageset to use [ 404 | mm | identicon | monsterid | wavatar ]
+  # @param string $r Maximum rating (inclusive) [ g | pg | r | x ]
+  # @return String containing either just a URL or a complete image tag
+  # @source http://gravatar.com/site/implement/images/php/
+  # 
+  def get_gravatar( email, s = 80, d = 'mm', r = 'g' ) 
+  	# get the email from URL-parameters or what have you and make lowercase
+    email_address = email.downcase
+
+    # create the md5 hash
+    hash = Digest::MD5.hexdigest(email_address)
+    
+    url = 'http://www.gravatar.com/avatar/'
+  	url += hash
+  	url += "?s=" + s.to_s() 
+  	url += "&d=" + d
+  	url += "&r=" + r
+  	
+  	return url
+  end
+  
+end
---
+++ src/ruby-sinatra/chat.rb
@@ -0,0 +1,64 @@
+require 'config.rb'
+require 'activity.rb'
+
+require 'sinatra'
+require 'pusher'
+require 'json'
+
+include Rack::Utils
+
+set :public_folder, '../'
+
+get '/' do
+  File.read('../index.html')
+end
+
+post '/chat' do
+  chat_info = params[:chat_info]
+
+  channel_name = nil
+
+  if( !chat_info )
+    status 400
+    body 'chat_info must be provided'
+  end
+
+  if( !request.referer )
+    status 400
+    body 'channel name could not be determined from request.referer'
+  end
+
+  channel_name = get_channel_name(request.referer)
+  options = sanitise_input(chat_info)
+
+  activity = Activity.new('chat-message', chat_info['text'], options)
+
+  data = activity.getMessage()
+  response = Pusher[channel_name].trigger('chat_message', data)
+
+  result = {'activity' => data, 'pusherResponse' => response}
+
+  status 200
+  headers \
+    'Cache-Control' =>  'no-cache, must-revalidate',
+    'Content-Type' =>  'application/json'
+
+  body result.to_json
+end
+
+def get_channel_name(http_referer)
+  pattern = /(\W)+/
+  channel_name = http_referer.gsub pattern, '-'
+  return channel_name
+end
+
+def sanitise_input(chat_info) 
+  email = chat_info['email']?chat_info['email']:''
+
+  options = {}
+  options['displayName'] = escape_html(chat_info['nickname'])
+  options['text'] = escape_html(chat_info['text'])
+  options['email'] = escape_html(email)
+  options['get_gravatar'] = true
+  return options
+end
